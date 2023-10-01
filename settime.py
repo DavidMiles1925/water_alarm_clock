4e0375b72cf6a7967f92b01293e290948298a218
DavidMiles1925 <davidmiles1925@gmail.com>
2023-10-01 09:44:50 -0500

    added button functionallity for setting time

--- settime.py
+++ settime.py
@@ -1,5 +1,28 @@
+import RPi.GPIO as GPIO
 from datetime import datetime
 import subprocess
+from lcd import lcd_init, lcd_text, LCD_LINE_1, LCD_LINE_2
+from utils import setup_pins, sleep, HOUR_BUTTON_PIN, MINUTE_BUTTON_PIN
+
+
+def get_user_date(date_int, label, min, max):
+    move_on = False
+
+    while move_on == False:
+                 #1234567890123456
+        lcd_text("SET:MIN ENTER:HR", LCD_LINE_1)
+        lcd_text(f"  {label}?: {date_int}", LCD_LINE_2)
+
+        if GPIO.input(MINUTE_BUTTON_PIN) == False:
+            date_int = date_int + 1
+
+        if date_int > max:
+            date_int = min
+
+        if GPIO.input(HOUR_BUTTON_PIN) ==  False:
+            sleep(0.2)
+            return date_int
+        
 
 def get_test_time():
     now = datetime.now()
@@ -34,27 +57,33 @@ def modify_single_digit_time(digit):
     return (new_digit)
 
 def press_enter_to_continue():
+
     print("Press Enter...")
     input("")
 
 if __name__ == "__main__":
+    setup_pins()
+
     year = 2023
     month = 1
     date = 1
     hour = 0
     minute = 0
 
-    print("SYSTEM TIME TEST")
-    press_enter_to_continue()
+    print("USE BUTTONS AND SCREEN")
+
+    year = get_user_date(year, "YEAR", 2023, 2030)
+    month = get_user_date(month, "MONTH", 1, 12)
+    day = get_user_date(date, "DAY", 1, 31)
+    hour = get_user_date(hour, "HOUR", 0, 23)
+    minute = get_user_date(minute, "MINUTE", 0, 59)
 
     system_time_string = create_date_string(year, month, date, hour, minute)
 
-    #os.system(f'sudo time --set "{system_time_string}"')
-    #os.system(f'sudo date -s {system_time_string}')
     subprocess.run(["sudo", "date", "-s", system_time_string])
-
-    press_enter_to_continue()
 
     get_test_time()
 
     press_enter_to_continue()
+
+    lcd_init()
