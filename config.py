7734886cfec8a8e9a67d5e695d21fa447fb48dab
David Miles <davidmiles1925@gmail.com>
2023-10-29 01:44:44 -0500

    pump testing

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 ALARM_HOUR = 0
 ALARM_MINUTE = 0
-ALARM_SET = False
-ALARM_DURATION = 3 # Time in seconds the pump will run
+ALARM_SET = True
+ALARM_DURATION = 0.1 # Time in seconds the pump will run.
+                        # In one second approx. 2oz of water comes through pump.
+
+DELAY_TIME = 30      # Time until the pump will activate again.
+                        # The max effective value is 59.
+
+SNOOZE_COUNTER = 2   # The number of extra times the alarm will go off without intervention
